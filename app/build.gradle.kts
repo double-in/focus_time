bf4de1d4fb02eadbd10b65fc28dcb720aa24a2aa
weiwei.xia <weiwei.xia@advance.ai>
2024-12-25 21:31:17 +0800

    colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.times.foucse_i"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
